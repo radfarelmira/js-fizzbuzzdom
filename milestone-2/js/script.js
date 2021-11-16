96cf4b088ed78cb9db8d1f298577aab5e4b43e62
radfarelmira <radfar.elmira@email.com>
2021-11-16 15:17:35 +0100

    add newbox to cilco

---
+++ milestone-2/js/script.js
@@ -0,0 +1,40 @@
+// Scrivi un programma che stampi i numeri da 1 a 100.
+// MILESTONE 1
+// Per i multipli di 3 stampi in console “Fizz” al posto del numero e per i multipli di 5 stampi in console Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi in console FizzBuzz.
+// MILESTONE 2
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+
+const boxesContainer = document.querySelector('.container');
+
+for ( let i = 1; i <= 100; i++){
+    console.log(i);
+
+    let print;
+    if ( i % 3 === 0 && i % 5 === 0 ){
+        print = ('FizzBuzz')
+        console.log(print);
+    } else if ( i % 3 === 0 ){
+        print = ('Fizz')
+        console.log(print);
+    } else if ( i % 5 === 0 ){
+        print = ('Buzz')
+        console.log(print);
+    } else{
+        print = (i)
+    }
+
+    const newBox = `
+          <div class="box">${print}</div>
+    `;
+    console.log(newBox)
+
+    boxesContainer.innerHTML += newBox
+
+    // const newBox = document.createElement ('div')
+    // newBox.innerHTML = (print)
+    // newBox.classList.add('box');
+    // console.log(newBox)
+
+    // boxesContainer.append (newBox)
+}
