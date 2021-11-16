22f29eb2c07764bfad4668d5f4e09c51d2a4e171
radfarelmira <radfar.elmira@email.com>
2021-11-16 15:51:11 +0100

    add boxColor milestone-3

---
+++ milestone-3/js/script.js
@@ -0,0 +1,48 @@
+// Scrivi un programma che stampi i numeri da 1 a 100.
+// MILESTONE 1
+// Per i multipli di 3 stampi in console “Fizz” al posto del numero e per i multipli di 5 stampi in console Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi in console FizzBuzz.
+// MILESTONE 2
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+// MILESTONE 3
+// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, 
+// per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
+
+
+const boxesContainer = document.querySelector('.container');
+
+for ( let i = 1; i <= 100; i++){
+    console.log(i);
+
+    let print;
+    let boxColor;
+    if ( i % 3 === 0 && i % 5 === 0 ){
+        print = ('fizzbuzz')
+        boxColor = ('fizzbuzz')
+        console.log(print);
+    } else if ( i % 3 === 0 ){
+        print = ('fizz')
+        boxColor = ('fizz')
+        console.log(print);
+    } else if ( i % 5 === 0 ){
+        print = ('buzz')
+        boxColor = ('buzz')
+        console.log(print);
+    } else{
+        print = (i)
+    }
+
+    const newBox = `
+          <div class="box ${boxColor}">${print}</div>
+    `;
+    console.log(newBox)
+
+    boxesContainer.innerHTML += newBox
+
+    // const newBox = document.createElement ('div')
+    // newBox.innerHTML = (print)
+    // newBox.classList.add('box');
+    // console.log(newBox)
+
+    // boxesContainer.append (newBox)
+}
