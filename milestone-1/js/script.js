86dd1833cf4e628a9e6ca4ece2d6567580a77f9c
radfarelmira <radfar.elmira@email.com>
2021-11-17 09:56:36 +0100

    correction

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -4,19 +4,17 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi in console FizzBuzz.
 
 for ( let i = 1; i <= 100; i++){
-    console.log(i);
-
-    let print;
+    
     if ( i % 3 === 0 && i % 5 === 0 ){
-        print = ('FizzBuzz')
-        console.log(print);
+        console.log('FizzBuzz');
+
     } else if ( i % 3 === 0 ){
-        print = ('Fizz')
-        console.log(print);
+        console.log('Fizz');
+
     } else if ( i % 5 === 0 ){
-        print = ('Buzz')
-        console.log(print);
+        console.log('Buzz');
+
     } else{
-        print = (i)
+        console.log(i);
     }
 }
